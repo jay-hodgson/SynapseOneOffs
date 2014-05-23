009eee29740192194d4eb56af90844dda7477528
Jay Hodgson <jay.hodgson@sagebase.org>
2014-05-23 11:15:12 -0700

    added program to help with sending a html message to a team

--- one-offs/src/main/java/org/sagebionetworks/LoginUtils.java
+++ one-offs/src/main/java/org/sagebionetworks/LoginUtils.java
@@ -3,10 +3,15 @@ package org.sagebionetworks;
 import static org.sagebionetworks.Constants.*;
 import org.sagebionetworks.client.SynapseClient;
 import org.sagebionetworks.client.SynapseClientImpl;
+import org.sagebionetworks.client.exceptions.SynapseException;
 
 public class LoginUtils {
 	public static SynapseClient createSynapseClient(String userAgent) {
 		return createSynapseClient(SESSION_TOKEN, PROD_REPO_URL, PROD_AUTH_URL, PROD_FILE_URL, userAgent);
+	}
+	
+	public static SynapseClient createSynapseClient(String username, String password, String userAgent) throws SynapseException {
+		return createSynapseClient(username, password, PROD_REPO_URL, PROD_AUTH_URL, PROD_FILE_URL, userAgent);
 	}
 	
 	public static SynapseClient createAdminStagingSynapseClient(String userAgent) {
@@ -35,8 +40,18 @@ public class LoginUtils {
 	}
 	
 	public static SynapseClient createSynapseClient(String sessionToken, String repo, String auth, String file, String userAgent) {
-		SynapseClient synapseClient = new SynapseClientImpl();		
+		SynapseClient synapseClient = new SynapseClientImpl();
 		synapseClient.setSessionToken(sessionToken);
+		synapseClient.setRepositoryEndpoint(repo);
+		synapseClient.setAuthEndpoint(auth);
+		synapseClient.setFileEndpoint(file);
+		synapseClient.appendUserAgent(userAgent);
+		return synapseClient;
+	}
+	
+	public static SynapseClient createSynapseClient(String username, String password, String repo, String auth, String file, String userAgent) throws SynapseException {
+		SynapseClient synapseClient = new SynapseClientImpl();
+		synapseClient.login(username, password);
 		synapseClient.setRepositoryEndpoint(repo);
 		synapseClient.setAuthEndpoint(auth);
 		synapseClient.setFileEndpoint(file);
---
+++ one-offs/src/main/java/org/sagebionetworks/SendMessageToTeam.java
@@ -0,0 +1,65 @@
+package org.sagebionetworks;
+
+import java.io.Console;
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.http.entity.ContentType;
+import org.sagebionetworks.client.SynapseClient;
+import org.sagebionetworks.repo.model.message.MessageToUser;
+
+public class SendMessageToTeam {
+	private static final String acuteId = "3320951";
+	
+	/**
+	 * Throw away program to attempt to change my email address (verified that it is currently not supported)
+	 */
+	public static void main(String[] args) {
+		try {
+			Console console = System.console();
+		    if (console == null) {
+		        System.out.println("Couldn't get Console instance");
+		        System.exit(0);
+		    }
+		    String username = console.readLine("Enter your Synapse username: ");
+		    char passwordArray[] = console.readPassword("Enter your Synapse password: ");
+		    String pw = new String(passwordArray);
+		    SynapseClient synapseClient = LoginUtils.createSynapseClient(username, pw, "Jay-SendingMessage");
+		    System.out.println("Successfully logged in.");
+			
+		    String subject = console.readLine("Enter message subject line: ");
+		    String path = console.readLine("Enter the path to the html file containing the message body: ");
+		    while (!new File(path).exists()) {
+		    	System.out.println("Could not find file, please try again.");
+		    	path = console.readLine("Enter the path to the html file containing the message body: ");
+		    }
+		    System.out.println("------------------------");
+		    System.out.println("Team ID - Team Name");
+		    System.out.println("3320951 - Acute Myeloid Leukemia Outcome Prediction DREAM Challenge Participants");
+		    System.out.println("3320895 - Broad DREAM Gene Essentiality Prediction Challenge");
+		    System.out.println("3319351 - Test Messaging Team");
+		    System.out.println("------------------------");
+		    String principalId = console.readLine("Enter the Team ID: ");
+		    
+		    String confirmation = console.readLine("Are you sure you want to send this message? [Y/N]");
+		    if ("y".equalsIgnoreCase(confirmation)) {
+			    byte[] htmlContent = Files.readAllBytes(Paths.get(path));
+				String fileHandleId = synapseClient.uploadToFileHandle(htmlContent, ContentType.create("text/html", StandardCharsets.UTF_8));
+				MessageToUser message = new MessageToUser();
+				message.setFileHandleId(fileHandleId);
+				message.setSubject(subject);
+				Set<String> recipientPrincipalIds = new HashSet<String>();
+				recipientPrincipalIds.add(principalId);
+				message.setRecipients(recipientPrincipalIds);
+				synapseClient.sendMessage(message);
+				System.out.println("Message Sent!");
+		    }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
