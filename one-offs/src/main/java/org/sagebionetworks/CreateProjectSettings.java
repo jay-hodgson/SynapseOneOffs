0339f8efe30e19f76e517fb588ea07c6a7919647
Jay Hodgson <jay.hodgson@sagebase.org>
2015-06-08 09:49:48 -0700

    create external s3 bucket project setting

--- one-offs/src/main/java/org/sagebionetworks/CreateProjectSettings.java
+++ one-offs/src/main/java/org/sagebionetworks/CreateProjectSettings.java
@@ -1,25 +1,23 @@
 package org.sagebionetworks;
 
-import java.io.BufferedReader;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
+import org.json.JSONObject;
+import org.mockito.cglib.core.CollectionUtils;
 import org.sagebionetworks.client.SynapseClient;
-import org.sagebionetworks.client.exceptions.SynapseException;
-import org.sagebionetworks.repo.model.ACCESS_TYPE;
-import org.sagebionetworks.repo.model.ACTAccessRequirement;
-import org.sagebionetworks.repo.model.PostMessageContentAccessRequirement;
-import org.sagebionetworks.repo.model.RestrictableObjectDescriptor;
-import org.sagebionetworks.repo.model.RestrictableObjectType;
-import org.sagebionetworks.repo.model.TermsOfUseAccessRequirement;
-import org.sagebionetworks.repo.model.file.ExternalUploadDestination;
-import org.sagebionetworks.repo.model.file.UploadDestination;
 import org.sagebionetworks.repo.model.file.UploadType;
+import org.sagebionetworks.repo.model.project.ExternalS3StorageLocationSetting;
 import org.sagebionetworks.repo.model.project.ExternalUploadDestinationSetting;
 import org.sagebionetworks.repo.model.project.ProjectSetting;
+import org.sagebionetworks.repo.model.project.ProjectSettingsType;
+import org.sagebionetworks.repo.model.project.S3UploadDestinationSetting;
+import org.sagebionetworks.repo.model.project.StorageLocationSetting;
 import org.sagebionetworks.repo.model.project.UploadDestinationListSetting;
 import org.sagebionetworks.repo.model.project.UploadDestinationSetting;
+import org.sagebionetworks.schema.adapter.org.json.EntityFactory;
 
 public class CreateProjectSettings {
 	
@@ -30,20 +28,45 @@ public class CreateProjectSettings {
 	 */
 	public static void main(String[] args) {
 		
-		String projectId = "syn2758121";
-		SynapseClient adminSynapseClient = LoginUtils.createAdminStagingSynapseClient("Jay-create-project-settings");
+		String projectId = "syn4230346";
+		SynapseClient adminSynapseClient = LoginUtils.createStagingSynapseClient("Jay-create-project-settings");
 		try {
+//			//validate the ability to update the project setting
+//			UploadDestinationListSetting currentSettings = (UploadDestinationListSetting)adminSynapseClient.getProjectSetting(projectId, ProjectSettingsType.upload);
+//			List<UploadDestinationSetting> currentDestinations = currentSettings.getDestinations();
+//			ExternalUploadDestinationSetting currentDestination = (ExternalUploadDestinationSetting)currentDestinations.get(0);
+//			currentDestination.setBanner("This banner text is customizable (in the upload destination settings).");
+//			currentDestination.setUrl("sftp://tcgaftps.nci.nih.gov/tcgapancan/tcgapancantestdir/synapse/jay");
+//			adminSynapseClient.updateProjectSetting(currentSettings);
+//			ProjectSetting settings = adminSynapseClient.getProjectSetting(projectId, ProjectSettingsType.upload);
+			
+//			List<StorageLocationSetting> mySettings = adminSynapseClient.getMyStorageLocationSettings();
+//			for (StorageLocationSetting storageLocationSetting : mySettings) {
+//				System.out.println("class = " + storageLocationSetting.getClass().getName());
+//				System.out.println("storage location id = " + storageLocationSetting.getStorageLocationId());
+//			}
+			
+			ExternalS3StorageLocationSetting externalSetting = new ExternalS3StorageLocationSetting();
+			externalSetting.setUploadType(UploadType.S3);
+			externalSetting.setBaseKey("jhodgson");
+			externalSetting.setBucket("external.bucket.test");
+			externalSetting = adminSynapseClient.createStorageLocationSetting(externalSetting);
+
 			UploadDestinationListSetting projectSetting = new UploadDestinationListSetting();
-			List<UploadDestinationSetting> destinations = new ArrayList<UploadDestinationSetting>();
-			ExternalUploadDestinationSetting setting = new ExternalUploadDestinationSetting();
-			setting.setBanner("This is required!");
-			setting.setUploadType(UploadType.SFTP);
-			setting.setUrl("sftp://localhost/project-config-test/");
-			destinations.add(setting);
-			projectSetting.setDestinations(destinations);
-			projectSetting.setSettingsType("upload");
+			projectSetting.setLocations(Collections.singletonList(externalSetting.getStorageLocationId()));
+			
+			
+////			setting.setUrl("sftp://fakeendpoint/test");
+////			setting.setUrl("sftp://tcgaftps.nci.nih.gov/tcgapancantestdir/synapse");
+//			setting.setUrl("sftp://ec2-54-245-147-42.us-west-2.compute.amazonaws.com/public/jay");
+////			setting.setUrl("sftp://localhost/synapse/jays-project-settings-test");
+			
+			projectSetting.setSettingsType(ProjectSettingsType.upload);
 			projectSetting.setProjectId(projectId);
+			JSONObject object = EntityFactory.createJSONObjectForEntity(projectSetting);
+			System.out.print(object.toString());
 			adminSynapseClient.createProjectSetting(projectSetting);
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		} 
